d9f35fd0d3a1177dd10352b5b853f727e05250a4
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-01-06 11:58:54 -0700

    add user_params

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_26_223832) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
