6ed0cc5618d6857ef4f9d2c8d67aa2bb5b2a2fcc
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-01-15 20:50:57 -0700

    create login

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get 'about', to: "pages#about"
   get 'signup', to: "users#new"
   resources :articles
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
 end
